fb6871edf80585b671ad473b370e6a860967f5e1
INDAHYANTI <indahyanti938@gmail.com>
2025-09-09 18:14:26 -1200

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,10 @@
+public class komentar {
+  public static void main(String[] args)
+  {
+  // Komentar satu baris
+        System.out.println("Ini komentar satu baris");
+
+        /* Komentar banyak baris
+        System.out.println("Ini komentar banyak baris");
+    }
+}
