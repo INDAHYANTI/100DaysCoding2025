b808256e73fc4e29c5f09a8e96da70aec7fa699e
INDAHYANTI <indahyanti938@gmail.com>
2025-09-10 20:53:55 -1200

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,14 @@
+public class Escape {
+
+  public static void main(String[] args)
+  {
+      System.out.println("halo\n semua");
+      System.out.println ("nama:\t indah ");
+      System.out.println("nama\t\t = indahyanti");
+      System.out.println("nim \t\t = d0224315");
+      System.out.println("kelas\t\t = b.24");
+      System.out.println("jeniskelamin\t = p");
+      System.out.println("hello\nword");
+        }
+    
+}
