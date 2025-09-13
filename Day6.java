56fc99c016754cff07976ffa773bc4a058212a29
INDAHYANTI <indahyanti938@gmail.com>
2025-09-13 18:09:48 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class Day5.java{
+    public static void main(String[] args) {
+        float beratBadan = 49.0f;     // float (kg)
+        double tinggiBadan = 159.0;   // double (cm)
+
+        System.out.println("Berat Badan  : " + beratBadan + " kg");
+        System.out.println("Tinggi Badan : " + tinggiBadan + " cm");
+    }
+}
