a12f4eef83233db702091683b9f8f618ac4f22b0
INDAHYANTI <indahyanti938@gmail.com>
2025-09-14 17:03:36 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,9 @@
+public class day7.java {
+    public static void main(String[] args) {
+        char huruf = 'B';       // contoh char
+        boolean status = false; // contoh boolean
+
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Status: " + status);
+    }
+}
