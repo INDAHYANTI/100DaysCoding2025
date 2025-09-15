b7dde4d52f985cce2f8f65274ad241b6858793e0
INDAHYANTI <indahyanti938@gmail.com>
2025-09-15 15:48:21 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,9 @@
+public class BelajarString {
+    public static void main(String[] args) {
+        // Membuat variabel String
+        String nama = "Indah";
+
+        // Menampilkan isi variabel
+        System.out.println("Halo, nama saya " + nama);
+    }
+}
