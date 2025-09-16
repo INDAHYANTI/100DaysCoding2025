32eb7cb7a24f83a62a25992e030caf152ddccce6
INDAHYANTI <indahyanti938@gmail.com>
2025-09-16 14:08:06 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,14 @@
+public class Day9.java {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int angka;
+        
+        // Inisialisasi variabel (memberikan nilai awal)
+        angka = 10;
+        System.out.println("Nilai awal angka: " + angka);
+        
+        // Update variabel (mengubah nilainya)
+        angka = 20;
+        System.out.println("Nilai setelah update: " + angka);
+    }
+}
