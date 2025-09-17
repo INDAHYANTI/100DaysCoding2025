c387dc42455322f564e5d14af360a408e0f6b7ad
INDAHYANTI <indahyanti938@gmail.com>
2025-09-17 12:28:35 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,11 @@
+public class KonstantaSederhana {
+    public static void main(String[] args) {
+        final double PI = 3.14;  // konstanta
+
+        int r = 5; // jari-jari lingkaran
+        double luas = PI * r * r;
+
+        System.out.println("Jari-jari = " + r);
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
