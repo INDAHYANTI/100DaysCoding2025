9bd576597db639f67e2b777e4f2b3c9fc5ca7ae3
INDAHYANTI <indahyanti938@gmail.com>
2025-09-22 18:19:36 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        
+        int angka1 = 10;
+        int angka2 = 5;
+
+        int hasilTambah = angka1 + angka2;
+      
+        int hasilKurang = angka1 - angka2;
+
+        System.out.println("Angka 1 = " + angka1);
+        System.out.println("Angka 2 = " + angka2);
+        System.out.println("Hasil Penjumlahan = " + hasilTambah);
+        System.out.println("Hasil Pengurangan = " + hasilKurang);
+    }
+}
