716b9477fba7f146298319ed6863c04b3f87b220
INDAHYANTI <indahyanti938@gmail.com>
2025-09-23 20:53:21 +0800

    Create 16day.java

---
+++ 16day.java
@@ -0,0 +1,14 @@
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 5;
+
+        // Perkalian
+        int hasilKali = angka1 * angka2;
+        System.out.println("Hasil Perkalian: " + hasilKali);
+
+        // Pembagian
+        int hasilBagi = angka1 / angka2;
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+    }
+}
