ca7f5a7b5748a1edb76521c6baa2aa1d5b1bcaea
INDAHYANTI <indahyanti938@gmail.com>
2025-09-24 20:47:35 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,13 @@
+public class OperatorModulus {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 3;
+
+        // operasi modulus
+        int hasil = angka1 % angka2;
+
+        System.out.println("Angka 1 = " + angka1);
+        System.out.println("Angka 2 = " + angka2);
+        System.out.println("Hasil dari " + angka1 + " % " + angka2 + " = " + hasil);
+    }
+}
