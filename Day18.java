3d80512d66b8b7cfab615afcd310a0cd53fbfa68
INDAHYANTI <indahyanti938@gmail.com>
2025-09-25 20:08:00 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,9 @@
+public class KonversiOtomatis {
+    public static void main(String[] args) {
+        int angka = 100;       // int (tipe data lebih kecil)
+        double hasil = angka;  // otomatis jadi double
+
+        System.out.println("Nilai int    : " + angka);
+        System.out.println("Nilai double : " + hasil);
+    }
+}
