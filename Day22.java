123d02926ad75918e7ca2d440af3a1bd937130d4
INDAHYANTI <indahyanti938@gmail.com>
2025-09-29 16:04:26 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,13 @@
+public class LuasPersegi {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel sisi
+        int sisi = 5;  
+
+        // Hitung luas (rumus: sisi × sisi)
+        int luas = sisi * sisi;
+
+        // Tampilkan hasil
+        System.out.println("Sisi persegi = " + sisi);
+        System.out.println("Luas persegi = " + luas);
+    }
+}
