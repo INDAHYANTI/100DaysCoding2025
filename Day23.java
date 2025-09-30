69618f0a91edad933898e1e2cd82dac5e6986556
INDAHYANTI <indahyanti938@gmail.com>
2025-09-30 13:59:17 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,15 @@
+public class LuasPersegiPanjang {
+    public static void main(String[] args) {
+        // Nilai panjang dan lebar langsung ditentukan
+        int panjang = 8;
+        int lebar = 5;
+
+        // Hitung luas
+        int luas = panjang * lebar;
+
+        // Tampilkan hasil
+        System.out.println("Panjang = " + panjang);
+        System.out.println("Lebar = " + lebar);
+        System.out.println("Luas persegi panjang = " + luas);
+    }
+}
