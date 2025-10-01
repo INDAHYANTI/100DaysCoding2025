353b9ab079f3da8b024f051aada682ab6ac5a0db
INDAHYANTI <indahyanti938@gmail.com>
2025-10-01 19:28:16 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,13 @@
+public class LuasLingkaran {
+    public static void main(String[] args) {
+        // Nilai jari-jari lingkaran
+        double r = 14;  
+
+        // Rumus luas = π × r × r
+        double luas = Math.PI * r * r;  
+
+        // Tampilkan hasil
+        System.out.println("Jari-jari lingkaran = " + r);
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
