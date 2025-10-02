f2dd802ab10f46e4ebc8b72566754a3d703a5529
INDAHYANTI <indahyanti938@gmail.com>
2025-10-02 20:39:08 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,18 @@
+public class IncrementDecrementLatihan {
+    public static void main(String[] args) {
+        int a = 5;  
+        int b = 10; 
+
+        a++;  a = 5 + 1 = 6
+        System.out.println("a = " + a); 
+
+        a++;  a = 6 + 1 = 7
+        System.out.println("a = " + a);
+
+        b--;  b = 10 - 1 = 9
+        System.out.println("b = " + b);
+
+        b--; b = 9 - 1 = 8
+        System.out.println("b = " + b);
+    }
+}
