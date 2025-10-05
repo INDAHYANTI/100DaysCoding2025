740a619e91878f9bc272add55c7ea3c4a39791c2
INDAHYANTI <indahyanti938@gmail.com>
2025-10-05 20:17:25 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,15 @@
+public class OperatorPerbandingan {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+
+        Operator ==
+        System.out.println (a == b);
+
+        Operator !=
+        System.out.println (a != b);
+    }
+}
