19f0993101fc89612eb5249bb627761b9f1537e9
INDAHYANTI <indahyanti938@gmail.com>
2025-10-06 16:49:41 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,16 @@
+public class Perbandingan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Menggunakan operator <
+        if (a < b) {
+            System.out.println(a + " lebih kecil dari " + b);
+        }
+
+        // Menggunakan operator >
+        if (b > a) {
+            System.out.println(b + " lebih besar dari " + a);
+        }
+    }
+}
