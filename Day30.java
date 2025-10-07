1e8604133cce7d13c7dcebba5fac6f53ca391cb9
INDAHYANTI <indahyanti938@gmail.com>
2025-10-07 16:07:57 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,12 @@
+public class Day030 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 15;
+        int c = 12;
+
+        System.out.println(a + " >= " + b + " = " + (a >= b));
+        System.out.println(a + " >= " + c + " = " + (a >= c));
+        System.out.println(a + " <= " + b + " = " + (a <= b));
+        System.out.println(a + " <= " + c + " = " + (a <= c));
+    }
+}
