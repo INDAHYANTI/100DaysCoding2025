dd17d0cc3d7f4bc54319a570642fd16d0f31b926
INDAHYANTI <indahyanti938@gmail.com>
2025-10-10 21:38:59 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,9 @@
+public class day33 {
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = true;
+
+        System.out.println(!a);
+        System.out.println(!b);
+    }
+}
