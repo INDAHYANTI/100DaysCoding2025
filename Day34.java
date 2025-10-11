784cffc0c278e4a4e280b173d3108afa7bead63d
INDAHYANTI <indahyanti938@gmail.com>
2025-10-11 23:34:48 +0800

    Update Day34.java

--- Day34.java
+++ Day34.java
@@ -1,9 +1,25 @@
 public class day034 {
     public static void main(String[] args) {
+        System.out.println("=== Mengkombinasikan Berbagai Operator ===");
         System.out.println("=== Mengecek Boleh Mengemudi ===");
-        int umur = 18;
-        boolean punyaSIM = true;
-        boolean bolehMengemudi = (umur >= 18 && punyaSIM == true);
+
+        int umurSekarang = 16;
+        int tambahanTahun = 2;
+        int umur = umurSekarang + tambahanTahun; 
+
+        int batasUmur = 18;
+
+        boolean punyaSIM = false;
+        boolean lulusTes = true;
+
+        
+        boolean bolehMengemudi = ((umur >= batasUmur) && (punyaSIM || lulusTes));
+
+        System.out.println("Umur sekarang: " + umurSekarang);
+        System.out.println("Tambahan tahun: " + tambahanTahun);
+        System.out.println("Umur setelah " + tambahanTahun + " tahun: " + umur);
+        System.out.println("Punya SIM: " + punyaSIM);
+        System.out.println("Lulus tes mengemudi: " + lulusTes);
         System.out.println("Boleh mengemudi: " + bolehMengemudi);
     }
 }
