d7d81523e34b6c9c5f72963fbf798a6b7a0fe734
INDAHYANTI <indahyanti938@gmail.com>
2025-10-12 15:04:16 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,11 @@
+public class day035 {
+    public static void main(String[] args) {
+        boolean hujan = true;
+
+        if (hujan == true) {
+            System.out.println("Bawa payung, karena sedang hujan.");
+        } else {
+            System.out.println("Tidak perlu payung, cuaca cerah.");
+        }
+    }
+}
