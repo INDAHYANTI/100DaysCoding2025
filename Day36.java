f0610d9674028155bde8a47f3e242ec3b318c59b
INDAHYANTI <indahyanti938@gmail.com>
2025-10-13 15:08:53 +0800

    Create Day36.java

---
+++ Day36.java
@@ -0,0 +1,13 @@
+public class Day36  {
+    public static void main(String[] args) {
+        int angka = 10;
+
+        if (angka > 0) {
+            System.out.println("Angka positif.");
+
+            if (angka % 2 == 0) {
+                System.out.println("Angka genap.");
+            }
+        }
+    }
+}
