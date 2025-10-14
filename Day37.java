f5918f522cf75f22daa79c85a5898bd03b3a3d13
INDAHYANTI <indahyanti938@gmail.com>
2025-10-14 13:09:32 +0800

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,10 @@
+public class Day37 {
+public static void main(String[] args) {
+        int angka = 15; 
+        if (angka % 2 == 0) {
+            System.out.println(angka + " adalah bilangan genap.");
+        } else {
+            System.out.println(angka + " adalah bilangan ganjil.");
+        }
+    }
+}
