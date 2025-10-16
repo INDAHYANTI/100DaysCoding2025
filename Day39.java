6ebad5c818355b7f7b00d5352a3a261688adfed5
INDAHYANTI <indahyanti938@gmail.com>
2025-10-16 21:37:45 +0800

    Refactor main method for drink ordering

--- Day39.java
+++ Day39.java
@@ -1,41 +1,10 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class day39 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int pilihan, harga = 0, gelas;
-        String menu = null;
-
-        System.out.println("## Menu Minuman ##");
-        System.out.println("1. Es Teh Manis\n2. Kopi Susu\n3. Jus Alpukat");
-        System.out.print("Pilih (1, 2, 3): ");
-        pilihan = input.nextInt();
-
-        if (pilihan == 1) {
-            menu = "Es Teh Manis";
-            harga = 5000;
-        } else if (pilihan == 2) {
-            menu = "Kopi Susu";
-            harga = 12000;
-        } else if (pilihan == 3) {
-            menu = "Jus Alpukat";
-            harga = 15000;
-        }
-
-        if (menu == null) {
-            System.out.println("Pilihan Salah ");
-        } else {
-            System.out.print("Jumlah Gelas : ");
-            gelas = input.nextInt();
-
-            System.out.println("=====================");
-            System.out.println("Menu Dipesan\t: " + menu);
-            System.out.printf("Harga\t\t: Rp %,d/gelas", harga);
-            harga = harga * gelas;
-            System.out.printf("\nTotal Harga\t: Rp %,d\n", harga);
-            System.out.println("Terima kasih sudah memesan");
-        }
-
-        input.close();
-    }
+  public static void main(String[] a) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("1.Es Teh(5000) 2.Kopi(12000) 3.Alpukat(15000)\nPilih: ");
+    int p=in.nextInt(),h=(p==1)?5000:(p==2)?12000:(p==3)?15000:0;
+    if(h==0) System.out.println("Salah!");
+    else {System.out.print("Jumlah: ");int g=in.nextInt();System.out.println("Total: Rp"+(h*g));}
+  }
 }
